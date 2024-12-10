e9e4e86154d6d5e6e1cceda70bbf58d6197c40d4
Praneeth <darlanka21_ug@ece.nits.ac.in>
2024-12-10 23:30:00 +0530

    Added : UC1 here we have initialized the player at start point 0

--- main.java
+++ main.java
@@ -1,5 +1,7 @@
 public class main {
 public static void main(String[] args){
     System.out.println("Snake and ladder Simulator!");
+    // uc1 initial_position is 0
+    int initial_position = 0;
 }
 }
