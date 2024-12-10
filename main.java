855ba4fd45fc0bdfbb878b8abdd20b0d2596bd95
Praneeth <darlanka21_ug@ece.nits.ac.in>
2024-12-10 23:35:20 +0530

    Added: uc2 generated a random number by the user between 1 to 6

--- main.java
+++ main.java
@@ -3,5 +3,7 @@ public static void main(String[] args){
     System.out.println("Snake and ladder Simulator!");
     // uc1 initial_position is 0
     int initial_position = 0;
+    int die = (int)(Math.random()*6) + 1;
+    System.out.println("the number throwed by the player : " + die);
 }
 }
